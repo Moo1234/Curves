816623d97848e49d48a654d172c618ef95213dc9
Moritz Martin <moritz.martin@uni-ulm.de>
2016-05-17 09:00:01 +0200

    Kommentare, Switch-Case

--- Curves/GameScene.swift
+++ Curves/GameScene.swift
@@ -8,7 +8,7 @@
 import SpriteKit
 
 
-
+//Pysics Categories
 struct PhysicsCat{
     static let p1Cat : UInt32 = 0x1 << 1
     static let gameAreaCat : UInt32 = 0x1 << 2
@@ -18,50 +18,57 @@ struct PhysicsCat{
 }
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
-    var lineContainer = SKNode()
-    var lineCanvas:SKSpriteNode?
+    
+    //***********************************************************************************
+    //***********************************************************************************
+    //                                  Variablen Start
+    //***********************************************************************************
+    //***********************************************************************************
+    
+    
+    //Curve + Properties
     var lineNode = SKShapeNode()
-    var linePhy = SKShapeNode()
-    
-    
-    var gapTimer = true
-    var gapLength = 0.17
-    var firstTime = true
-    
-    
-    var texture = SKTexture()
-    var gameArea = SKShapeNode()
-    var dead = false
-    
-    var linePoints = [SKShapeNode()]
-    
-    var leftBtn = SKShapeNode()
-    var rightBtn = SKShapeNode()
-    
-    var lastDrawTime:Int64 = 0
-    var lineCount = 0
-    var timeScan:Int64 = 0
-    var path = CGPathCreateMutable()
-    
     var lastPoint = CGPointZero
-    
     var wayPoints: [CGPoint] = []
-    
     let p1 = SKShapeNode(circleOfRadius: 3.0)
     var xCurve: CGFloat = 1.0
     var yCurve: CGFloat = 1.0
-    var lineThickness: CGFloat = 5.0
+    var path = CGPathCreateMutable()
     
+    
+    
+    //Layout
+    var texture = SKTexture()
+    var gameArea = SKShapeNode()
+    var dead = false
+    var leftBtn = SKShapeNode()
+    var rightBtn = SKShapeNode()
+    var btnWidth:CGFloat = 30.0
+    var lineContainer = SKNode()
+    var lineCanvas:SKSpriteNode?
+
+   
+    
+    //Timer
     var myTimer1 : NSTimer = NSTimer()
     var myTimer2 : NSTimer = NSTimer()
     var jumpTimer: NSTimer = NSTimer()
     
     
-    var btnWidth:CGFloat = 30.0
-   
     
+    // Vars for Items
+    var item = SKSpriteNode()
+    var lineThickness: CGFloat = 5.0
+    var gapTimer = true
+    var gapLength = 0.17
     var test = CGFloat(1)
     var itemList = [SKSpriteNode]()
+    
+    //***********************************************************************************
+    //***********************************************************************************
+    //                                  Variablen End
+    //***********************************************************************************
+    //***********************************************************************************
     
     
     
@@ -117,23 +124,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         lineContainer.addChild(lineCanvas!)
         lastPoint = CGPointMake(view.frame.size.width/2.0, view.frame.size.height/2.0)
         
-        
-        
-//        physicsBody = SKPhysicsBody(polygonFromPath: path)
-//        physicsBody?.categoryBitMask = PhysicsCat.p1TailCat
-//        physicsBody?.contactTestBitMask = PhysicsCat.p1Cat
-        
-        
-        
-        //        wayPoints.append(CGPoint(x: 100,y: 100))
-        //        let targetPoint = wayPoints[0]
-        //        changeDirection(targetPoint)
-        
-        
-        
     }
     
     
+    
+    //***********************************************************************************
+    //***********************************************************************************
+    //                                  Functions Start
+    //***********************************************************************************
+    //***********************************************************************************
+    
+    
+    
+    // Für Kurve -> Punkt zu Radius
     func pointToRadian(targetPoint: CGPoint) -> Double{
         let deltaX = targetPoint.x;
         let deltaY = targetPoint.y;
@@ -141,10 +144,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         return ( Double(rad) * (180 / M_PI))
     }
     
+    //Für Kurve -> Radius zu Punkt
     func radianToPoint(rad: Double) -> CGPoint{
         return CGPoint(x: cos(rad*(M_PI/180))*141, y: sin(rad*(M_PI/180))*141)
     }
     
+    
+    
+    //Für Kurve + Geschwindigkeit
     func changeDirection(targetPoint: CGPoint){
         let currentPosition = position
         let offset = CGPoint(x: targetPoint.x - currentPosition.x, y: targetPoint.y - currentPosition.y)
@@ -174,18 +181,22 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
         
     }
+    
+    
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
         myTimer1.invalidate()
         myTimer2.invalidate()
     }
     
-    
+    //Linkskurve
     func changeDirectionL(){
         //        print(timer.userInfo)
         let alt = pointToRadian(wayPoints[0])
         wayPoints[0] = radianToPoint(alt+5)
         changeDirection(wayPoints[0])
     }
+    
+    //Rechtskurve
     func changeDirectionR(){
         let alt = pointToRadian(wayPoints[0])
         wayPoints[0] = radianToPoint(alt-5)
@@ -194,6 +205,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     
+    //Erstellt + zeichnet linie, macht Lücken
     func drawLine() {
         if (CGPathIsEmpty(path)) {
             // Create a new line that starts where the previous line ended
@@ -212,9 +224,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             }
             
         }
-        // Add a random line segment
-        
-        
         var x = lastPoint.x + xCurve
         var y = lastPoint.y + yCurve
         CGPathAddLineToPoint(path, nil, x, y)
@@ -223,22 +232,20 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         p1.position = CGPoint(x: x, y: y)
         lastPoint = CGPointMake(x, y)
         wayPoints.append(CGPoint(x:x,y:y))
-        
     }
     
     func makeHole(){
         gapTimer = true
     }
     
-    var i = 0
+    
+    
+    
     override func update(currentTime: CFTimeInterval) {
-        
-       
-        
+
         var rand = arc4random() % 500
         if rand == 10{
 
-            
             makeRandomItems()
         }
         
@@ -246,13 +253,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             drawLine()
             addLinesToTexture()
         }
-        
-       
-       
-        
+  
     }
 
-    var item = SKSpriteNode()
+    //Erstellt zufällige Items, mit zufälligen Positionen
     func makeRandomItems(){
         
         var pos = CGPoint()
@@ -272,14 +276,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         //wenn es mehr Items gibt, zahl erhöhen
         var nameRandom = 1 + arc4random() % 2
-        if nameRandom == 1{
+        
+        switch nameRandom{
+        
+        case 1:
             imageName = "testItem"
-        }else if nameRandom == 2{
+        case 2:
             imageName = "fatItem"
-            //weitere Namen...wenn es mal items gibt
-        }//else if...
-        
-        
+            
+        //wenn mehr Items
+        //case 3:...
+        default:
+            break
+        }
         item = SKSpriteNode(imageNamed: imageName)
         item.name = imageName
         item.setScale(0.6)
@@ -293,9 +302,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             addChild(item)
             itemList.append(item)
         }
-        
-        
     }
+    
     
     func addLinesToTexture () {
         // Convert the contents of the line container to an SKTexture
@@ -306,46 +314,49 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     func didBeginContact(contact: SKPhysicsContact) {
-        //print("dead " , contact.bodyA == p1.physicsBody , " " , contact.bodyB)
-        
-        //print(contact.bodyA.node?.position , contact.bodyB.node?.position)
-      
-        
-        
-        
+
         if (contact.bodyA.categoryBitMask == PhysicsCat.itemCat) || contact.bodyB.categoryBitMask == PhysicsCat.itemCat{
             for var i = 0; i < itemList.count; i = i+1{
                 if contact.bodyB.node!.position == itemList[i].position{
                     itemList[i].removeFromParent()
                     
-                    if contact.bodyB.node!.name == "testItem"{
+                    switch contact.bodyB.node!.name!{
+                    
+                    case "testItem":
                         test = 2
                         gapLength = 0.09
                         xCurve = xCurve * 2
                         yCurve = yCurve * 2
                         _ = NSTimer.scheduledTimerWithTimeInterval(5.0, target: self, selector: #selector(GameScene.lowerSpeed), userInfo: nil, repeats: false)
-                    }else if contact.bodyB.node?.name == "fatItem"{
+                    case "fatItem":
                         lineThickness = lineThickness + 4.0
                         gapLength = gapLength + 0.05
                         _ = NSTimer.scheduledTimerWithTimeInterval(8.0, target: self, selector: #selector(GameScene.lowerThickness), userInfo: nil, repeats: false)
+                    default:
+                        break
                     }
+                    
+                    
                 }
                 
             }
-
-          
         }else{
             print("dead")
             dead = true
             //      p1.physicsBody?.dynamic = false
         }
-        
-        
+   
     }
         
     func didEndContact(contact: SKPhysicsContact) {
         print("Yo")
     }
+    
+    //***********************************************************************************
+    //***********************************************************************************
+    //                                  Item-Functions Start
+    //***********************************************************************************
+    //***********************************************************************************
     
     func lowerSpeed(){
         test = 1
@@ -359,6 +370,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         gapLength = gapLength-0.05
     }
 
+    //***********************************************************************************
+    //***********************************************************************************
+    //                                  Functions End
+    //***********************************************************************************
+    //***********************************************************************************
     
 }
 
