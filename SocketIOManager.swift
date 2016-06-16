c8bf3591fc04ee27162f688a12149f0741948de6
Moritz Martin <moritz.martin@uni-ulm.de>
2016-06-16 11:11:15 +0200

    SocketIO

---
+++ SocketIOManager.swift
@@ -0,0 +1,13 @@
+//
+//  SocketIOManager.swift
+//  Curves
+//
+//  Created by Moritz Martin on 16.06.16.
+//  Copyright © 2016 Moritz Martin. All rights reserved.
+//
+
+import UIKit
+
+class SocketIOManager: NSObject {
+
+}
